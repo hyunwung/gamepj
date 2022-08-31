4dcb8b23cb8f49926fe64dff40f3f9d462a8c571
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-01 00:36:39 +0900

    Update 라우터

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import { Route,Routes } from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <div className='App'>
+      <Routes>
+        <Route></Route>
+      </Routes>
+    </div>
   )
 }
 
