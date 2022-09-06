ab83fe4fccafbc8389c21308cb80071d614523d5
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-09-06 23:11:49 +0900

    Update 헤더 업데이트

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
+import "./Header.scss"
 
 const Header = () => {
   return (
-    <div>헤더</div>
+    <div className='header'>
+      <div className='header-container'>
+        <div className='header-left'></div>
+        <div className='header-right'></div>
+      </div>
+    </div>
   )
 }
 
