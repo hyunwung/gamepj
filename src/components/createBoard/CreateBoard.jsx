29153fc159397d8074987901007aea9b3ac7053c
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-11-09 17:14:46 +0900

    update

--- src/components/createBoard/CreateBoard.jsx
+++ src/components/createBoard/CreateBoard.jsx
@@ -1,19 +1,15 @@
 import React, { useState } from 'react'
 import "./CreateBoard.scss"
+import { TextEditor } from '../textEditor/TextEditor'
 
 const CreateBoard = () => {
     const [data,setData] = useState("")
     
     return (
     <div className='createBoard'>
-        <div className='createBoard-container'>
-        <label>제목</label>
-        <input type="file" class="inputImage" id="img1" name="1" accept="image/*"/>
-        <label>내용</label>
-        <textarea></textarea>
-        <label>이미지</label>
-        <input type="image"/>
-        </div>
+      <div className='createBoard-container'>
+        <TextEditor></TextEditor>
+      </div>
     </div>
   )
 }
