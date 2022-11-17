afe08b3d7815ca3322208fdca088a909243e916b
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-11-18 08:03:58 +0900

    Update

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -4,15 +4,19 @@ import "./Comment.scss"
 const Comment = () => {
   return (
     <div className='comment'>
-        <hr style={{opacity:"0.3",color:"gray",margin:"15px 5px 15px 24px"}}></hr>
-        <span className='comment-count'>댓글 2</span>
-        <div className='comment-container'>
-          <div className='comment-username'>
-            <span>현웅</span>
-          </div>
-          <textarea></textarea>
-          <a><button>등록</button></a>
+      <hr style={{opacity:"0.3",color:"gray",margin:"15px 5px 15px 24px"}}></hr>
+      <span className='comment-count'>댓글 2</span>
+      <div className='comment-container'>
+        <div className='comment-username'>
+          <span>현웅</span>
         </div>
+        <textarea></textarea>
+        <a><button>등록</button></a>
+      <hr></hr>
+      <div className='other-comment'>
+        
+      </div>
+      </div> 
     </div>
   )
 }
