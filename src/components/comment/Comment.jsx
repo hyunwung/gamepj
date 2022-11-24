03c99adff46fb0512b718af9e854419512f519df
hyunwungSEO <79622280+hyunwung@users.noreply.github.com>
2022-11-24 20:07:32 +0900

    Update

--- src/components/comment/Comment.jsx
+++ src/components/comment/Comment.jsx
@@ -7,17 +7,25 @@ const Comment = () => {
       <hr style={{opacity:"0.3",color:"gray",margin:"15px 5px 15px 24px"}}></hr>
       <span className='comment-count'>댓글 2</span>
       <div className='comment-container'>
-        <div className='comment-username'>
+        {/* <div className='comment-username'>
           <span className='comment-profile'>현웅</span>
-        </div>
+        </div> */}
         <textarea></textarea>
         <a><button>등록</button></a>
       </div>
       <div className='other-comment'>
-        <span className='comment-profile'>하림</span> &nbsp; |  &nbsp; 
-        <span>2022. 10. 15. &nbsp; 15:30 </span>
-        <button className='comment-reply'>답글</button>
-        <span className='comment-comments'>그냥 댓글1</span>
+        {/* <div className='comment-username'>
+          <span className='comment-profile'>하림</span>
+        </div> */}
+        <div className='comment-info'>
+          <span className='comment-username'>나는 형식</span>|
+          <span className='comment-date'>2022. 10. 15. &nbsp; 15:30 </span>
+          <button className='comment-reply'>답글</button>
+        </div>
+        <div className='comment-bottom'>
+          <p className='other-comment-content'>여기 댓글과 내용들이 작성됩니다</p>  
+        </div>
+        <a href='/' className='comment-answer'>답글 2개 모두 보기</a>
       </div>
     </div>
   )
