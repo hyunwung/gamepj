99dcf51531b49e363d1a167f264efaa8d6c06ab0
hyunwungSEO <sddsk1123@naver.com>
2023-01-03 20:25:08 +0900

    Update 프로필 페이지 업데이트

---
+++ src/components/profileHeader/ProfileHeader.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ProfileHeader = () => {
+  return (
+    <div className='profileHeader'>
+      <div className='profileHeader-container'>
+        <div>프로필 이미지</div>
+        <span>닉네임</span>
+        <span>내가 쓴 글</span>
+        <span>내가 쓴 댓글</span>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileHeader
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import ProfileHeader from '../components/profileHeader/ProfileHeader'
 
 const Profile = () => {
   return (
     <div className='profile'>
+      <ProfileHeader></ProfileHeader>
     </div>
   )
 }
