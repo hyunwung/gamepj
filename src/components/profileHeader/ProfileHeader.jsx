5d46ef1b47dd6a9bcb67507258ccf981f258694e
hyunwungSEO <sddsk1123@naver.com>
2023-01-16 14:07:40 +0900

    FEAT 프로필 페이지

--- src/components/profileHeader/ProfileHeader.jsx
+++ src/components/profileHeader/ProfileHeader.jsx
@@ -8,11 +8,36 @@ const ProfileHeader = () => {
       <Header></Header>
       <div className='profileHeader-container'>
         <div className='profile-left'>
-          <div className='profile-logo'>이미지</div>
-          <span>닉네임</span>
+          <div className='profile-logo'></div>
+          <div className='profile-modi'>
+            <input placeholder='닉네임' type="text"></input>
+            <button>변경</button>
+          </div>
+          <span className='profile-exit'>회원탈퇴</span>
         </div>
         <div className='profile-right'>
-          d
+          <div className='profile-items'>
+            <span className='profile-item'>작성글</span>
+            <span className='profile-item'>작성댓글</span>
+            <span className='profile-item'>좋아요한 글</span>
+          </div>
+          <hr className='line1'></hr>
+          <span className='profile-title'>댓글 || 작성글</span>
+          <hr className='line2'></hr>
+          {/* 댓글 or 작성글 */}
+          <div className='profile-content'>
+            <div className='profile-content-container'>
+              <input type="checkbox" className='profile-checkbox'></input>
+              <div>댓글 혹은 작성글 제목</div>
+            </div>
+            <div className='profile-content-date'>2023.01.15</div>
+          </div>
+          {/* 여기까지 */}
+          <hr className='line2'></hr>
+          <div className='profile-entire'>
+            <input type="checkbox"></input>
+            <span>전체선택</span>  
+          </div>
         </div>
         {/* <span>내가 쓴 글</span>
         <span>내가 쓴 댓글</span> */}
