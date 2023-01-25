65f0e5af1bdfba77745f9e65dd1d2895e8171773
hyunwungSEO <sddsk1123@naver.com>
2023-01-25 10:26:25 +0900

    update 전역 css

--- src/components/loginContainer/LoginContainer.jsx
+++ src/components/loginContainer/LoginContainer.jsx
@@ -10,7 +10,6 @@ const LoginModal = () => {
         <div className='login-bottom-container'>
             <span className='login-title'>RSD</span>        
             <button className='social-login-btn'><span>Google Play로 로그인</span></button>
-            <button className='social-login-btn'><span>KAKAO로 로그인</span></button>
         </div>
     </div>
   )
