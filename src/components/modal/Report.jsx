c8d4dcd39a2e369b388edf03b1cd0224c165ac6d
hyunwungSEO <sddsk1123@naver.com>
2023-01-31 20:40:56 +0900

    feat 신고 기능 작업

--- src/components/modal/Report.jsx
+++ src/components/modal/Report.jsx
@@ -12,7 +12,6 @@ const Report = ({modalIsOpen , setModalIsOpen}) => {
 	const [select,setSelect] = useState(0)
 
 	const handleSelect = (id) =>{
-		console.log("클릭?")
 		setSelect(id)
 	}
     return (
@@ -33,47 +32,47 @@ const Report = ({modalIsOpen , setModalIsOpen}) => {
 					<h1 className='reason-title'>사유선택</h1>
 					<ul>
 						<li className='reason-item' onClick={()=>handleSelect(0)}>
-							{select === 0 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 0 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>스팸홍보/도배글입니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(1)}>
-							{select === 1 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 1 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>음란물입니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(2)}>
-							{select === 2 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 2 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>불법정보를 포함하고 있습니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(3)}>
-							{select === 3 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 3 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>청소년에게 유해한 내용입니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(4)}>
-							{select === 4 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 4 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>욕설/생명경시/혐오/차별적 표현입니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(5)}>
-							{select === 5 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 5 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>개인정보 노출 게시물입니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(6)}>
-							{select === 6 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 6 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>불쾌한 표현이 있습니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(7)}>
-							{select === 7 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 7 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>명예훼손 또는 저작권이 침해되었습니다.</span>
 						</li>
 						<li className='reason-item' onClick={()=>handleSelect(8)}>
-							{select === 8 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'></div>}
+							{select === 8 ? <div className='reason-select'><AiOutlineCheck style={{color:"white",fontSize:"20px"}}></AiOutlineCheck></div> : <div className='none-select'><AiOutlineCheck style={{color:"#dcdcdc",fontSize:"20px"}}></AiOutlineCheck></div>}
 							<input type="radio" name="select" value="0" className="report-reasons"></input>
 							<span className='report-reason-items'>불법촬영물등이 포함되어 있습니다.</span>
 						</li>
