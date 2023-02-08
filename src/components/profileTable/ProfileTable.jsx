7be46ebb55812daa1a45f21f04ae51dc3312e972
hyunwungSEO <sddsk1123@naver.com>
2023-02-08 17:13:23 +0900

    테이블 수정

--- src/components/profileTable/ProfileTable.jsx
+++ src/components/profileTable/ProfileTable.jsx
@@ -7,7 +7,7 @@ const ProfileTable = () => {
   const handleTitle = (id) => {
     setTitle(id)
   }
-  const [data,setData] = useState(1)
+  const [data,setData] = useState(null)
   return (
     <div className='profileTable'>
       <div className='profileTable-container'>  
@@ -16,87 +16,71 @@ const ProfileTable = () => {
           <span className='profile-item' onClick={()=>handleTitle(1)}>작성댓글</span>
           <span className='profile-item' onClick={()=>handleTitle(2)}>좋아요한 글</span>
         </div>
-        <hr className='line1'></hr>
+        
         <table className='profile-table'>
           <thead>
-            {title === 0 ? <tr>
+            {title === 0 ? 
+            <tr>
               <th scope='col'>제목</th>
               <th scope='col'>작성일</th>
               <th scope='col'>조회수</th>
             </tr> : null}
-            {title === 1 ? <tr>
-              <th scope='col'>댓글</th>
+            {title === 1 ? 
+            <tr>
+              <th>댓글</th>
             </tr> : null}
-            {title === 2 ? <tr>
-              <th scope='col'>제목</th>
-              <th scope='col'>작성일</th>
-              <th scope='col'>작성자</th>
-              <th scope='col'>조회</th>
+            {title === 2 ? 
+            <tr>
+              <th>제목</th>
+              <th>작성일</th>
+              <th>작성자</th>
+              <th>조회</th>
             </tr> : null}
           </thead>
           <tbody>
-            {title === 0 ? 
+            {data === null && title === 0 ?
               <tr>
-                <td>
+                <td className='profile-comment-null' colSpan="3">작성하신 글이 없습니다.</td>
+              </tr> : null}
+            {data !== null && title === 0 ? 
+              <tr>
+                <td className='td-1'>
                   <input type="checkbox" className='profile-checkbox'></input>
                   <span className='profile-content-title'>글 제목@@@</span>
                 </td>
-                <td>
+                <td className='td-2'>
                   <span className='profile-content-title-date'>2023.01.15</span>
                 </td>
-                <td>
+                <td className='td-3'>
                   <span className='profile-content-title-view'>155</span>
                 </td>
               </tr>
             : null}
-            {title === 1 ? 
+            {data === null && title === 1 ? 
               <tr>
-                {data === null && title === 1 ?
-                  <td>
-                    <span className='profile-comment-null'>작성하신 댓글이 없습니다.</span>
-                  </td>
-                    : null}
-                {data !== null && title === 1 ?
-                    <td>
-                      <div className='profile-type2'>
-                        <div className='profile-type2-container'>
-                          <input type="checkbox" className='profile-checkbox'></input>
-                          <div>댓글 1</div>
-                        </div>
-                        <div className='profile-content-date'>2023.01.15</div>
-                      </div>
-                      <hr className='line2'></hr>
-                    </td> : null}
-              </tr>
-            : null}
-            {title === 2 ? 
-              <div>
-                {data === null && title === 2 ?
-                  <tr>
-                    <td>
-                      <span className='profile-comment-null'>작성하신 댓글이 없습니다.</span>
-                    </td>
-                  </tr>
-                  : null}
-                {data !== null && title === 2 ?
-                  <tr>
-                    <td>
-                      <input type="checkbox" className='profile-checkbox'></input>
-                      <span className='profile-content-title'>글 제목</span>
-                    </td>
-                    <td>
-                      <span className='profile-content-title-date'>2023.01.15</span>
-                    </td>
-                    <td>
-                      <span className='profile-content-title-date'>김근육</span>
-                    </td>
-                    <td>
-                      <span className='profile-content-title-view'>155</span>
-                    </td>
-                  </tr>
-                : null}
-              </div>
-            : null}
+                <td className='profile-comment-null'>작성하신 댓글이 없습니다.</td>
+              </tr> : null}
+            {data !== null && title === 1 ?
+              <tr>
+                <td>댓글</td>
+              </tr> : null}
+            {data === null && title === 2 ? 
+              <tr>
+                <td className='profile-comment-null' colSpan="4">작성하신 댓글이 없습니다.</td>
+              </tr> : null}
+            {data !== null && title === 2 ?
+            <tr>
+              <td className='td-1'>
+                <input type="checkbox" className='profile-checkbox'></input>
+                <span className='profile-content-title'>글 제목@@@</span>
+              </td>
+              <td className='td-2'>
+                <span className='profile-content-title-date'>2023.01.15</span>
+              </td>
+              <td className='td-3'>
+                <span className='profile-content-title-view'>155</span>
+              </td>
+            </tr> : null}
           </tbody>
         </table>
        </div>
