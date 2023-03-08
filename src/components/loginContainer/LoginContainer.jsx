f733afd1ff903792a0aff004761022f4496e672f
user08 <user08@ill.com>
2023-03-08 18:48:09 +0900

    update

--- src/components/loginContainer/LoginContainer.jsx
+++ src/components/loginContainer/LoginContainer.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
 import "./LoginContainer.scss"
-
-const LoginModal = () => {
+import {GoogleLogin ,GoogleOAuthProvider} from '@react-oauth/google';
+const LoginContainer = () => {
+    const onSuccess = (response) => {
+        console.log(response);
+        // const params = new URLSearchParams();
+        // console.log(params.append("idToken", response.tokenObj.id_token))
+        //localStorage.setItem("accessToken",response.tokenObj.id_token);
+    }
+    const onFailure = (error) => {
+        console.log(error);
+    }
     const openGoogle = () => {
         // 토큰 불러오고
-        window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=code&scope=${process.env.REACT_APP_GOOGLE_SCOPE}`
+        window.location.href = `https://accounts.google.com/o/oauth2/auth?client_id=${process.env.REACT_APP_GOOGLE_CLIENT_ID}&redirect_uri=${process.env.REACT_APP_GOOGLE_REDIRECT_URI}&response_type=token&scope=${process.env.REACT_APP_GOOGLE_SCOPE}`
     };
     return (
     <div className='login-container'>
@@ -12,11 +21,18 @@ const LoginModal = () => {
             <a className='login-link' href='/main'><span className='login-logo'>RSD</span></a>
         </div>
         <div className='login-bottom-container'>
-            <span className='login-title'>RSD</span>        
-            <button type="button" className='social-login-btn' onClick={openGoogle}><span>Google Play로 로그인</span></button>
+            <span className='login-title'>RSD</span>
+            <GoogleOAuthProvider clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID} className='social-login-btn'>
+                <GoogleLogin
+                    onSuccess={onSuccess}
+                    onError={() => {
+                        console.log('Login Failed');
+                    }}></GoogleLogin>
+            </GoogleOAuthProvider>
+            {/* <button type="button" className='social-login-btn' onClick={openGoogle}><span>Google Play로 로그인</span></button> */}
         </div>
     </div>
   )
 }
 
-export default LoginModal
+export default LoginContainer
