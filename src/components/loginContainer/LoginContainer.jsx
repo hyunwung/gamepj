df584d3baefdf33d979ca03e13fef784b44b0c38
hyunwungSEO <sddsk1123@naver.com>
2023-03-08 20:25:06 +0900

    merge 0308

--- src/components/loginContainer/LoginContainer.jsx
+++ src/components/loginContainer/LoginContainer.jsx
@@ -1,11 +1,33 @@
 import React from 'react'
 import "./LoginContainer.scss"
+import { useNavigate ,useSearchParams} from 'react-router-dom';
+import { useEffect } from 'react';
 
 const LoginModal = () => {
+    const navigate = useNavigate();
+    const [searchParams, setSearchParams] = useSearchParams();
     const openGoogle = () => {
-        window.location.href = process.env.REACT_APP_GOOGLE_CLIENT_ID
+        // window.location.href = "http://ec2-15-165-122-126.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google"
+        // const nextStep = async () => {
+        //     await navigate("/oauth/redirect", { state: { url: "http://ec2-15-165-122-126.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google" } })
+        // }
+        
+        const GOOGLE_CODE = searchParams.get("token")
+        localStorage.setItem("token",GOOGLE_CODE)
+        //navigate("/oauth/redirect");
+        fetch(`http://ec2-15-165-122-126.ap-northeast-2.compute.amazonaws.com:8080/oauth2/authorization/google`)
+        .then(res=>res.json())
+        .then(data=>{
+            localStorage.setItem("token",data)
+            //navigate("/oauth/redirect");
+        })
         // 토큰 불러오고
     };
+    useEffect(()=>{
+        // setTimeout(()=>{
+        //     navigate("/oauth/redirect",{replace:true})
+        // })
+    },[])
     return (
     <div className='login-container'>
         <div className='login-header'>
