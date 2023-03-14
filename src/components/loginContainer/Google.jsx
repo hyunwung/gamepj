970e3822e0920ff39f5331ce53e69651cea6dc74
hyunwungSEO <sddsk1123@naver.com>
2023-03-14 21:01:35 +0900

    update

--- src/components/loginContainer/Google.jsx
+++ src/components/loginContainer/Google.jsx
@@ -1,30 +1,38 @@
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { useNavigate, useSearchParams } from "react-router-dom";
+import {useCookies} from "react-cookie"
 import Swal from "sweetalert2";
 
 const Google = () => {
   const [searchParams, setSearchParams] = useSearchParams();
+  const [cookies, setCookie] = useCookies(['refresh_token']); // 쿠키 훅 
+  const settingCookie = () => {
+    console.log(cookies)
+    //setCookie('name', newName, { path: '/' });
+  }
   const [token,setToken] = useState('')
   const navigate = useNavigate();
 
   const googleLogin = async () => {
-    try {
+    try{
         const response = await axios.get("/users/me",{
           headers:{
             'Authorization': 'Bearer '+searchParams.get("token")
           }
         });
-        const user = response.data.body.user.id  
+        console.log(response)
+        const user = response.data.body.user.id
+        settingCookie('refresh_token')
         localStorage.setItem("accessToken", searchParams.get("token"));
         localStorage.setItem("user", user);
         
-        if(user.length > 8){
-          Swal.fire({title:"닉네임을 2자~6자로 변경해주세요"})
-          navigate("/profile")
-        }else{
-          navigate("/main");
-        }
+        // if(user.length > 8){
+        //   Swal.fire({title:"닉네임을 2자~6자로 변경해주세요" ,confirmButtonColor:"#488bf4"})
+        //   navigate("/profile")
+        // }else{
+        //   navigate("/main");
+        // }
     }catch (error) {
       console.log(error)
     }
